f574bcf109e7a8a397ce3a9f0208e6e68d1f5fe3
zaniyar <jaha@zhaw.ch>
2019-12-12 16:20:51 +0100

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MachineComponent } from './machine/machine.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [ {
+  path: 'machines',
+  component: MachineComponent,
+  data: { title: 'Machine List' }
+}, ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSliderModule } from '@angular/material/slider';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { MachineComponent } from './machine/machine.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
     MachineComponent,
   ],
   imports: [
@@ -23,6 +26,8 @@ import { MachineComponent } from './machine/machine.component';
     BrowserAnimationsModule,
     MatSliderModule,
     MatButtonModule,
+    MatCardModule,
+    FlexLayoutModule,
     HttpClientModule,
   ],
   providers: [],
