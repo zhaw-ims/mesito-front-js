0f024a281031d49b693bb2320a2bcfe06d8f73d7
zaniyar <jaha@zhaw.ch>
2019-12-12 16:19:53 +0100

    Add machine service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +23,7 @@ import { MachineComponent } from './machine/machine.component';
     BrowserAnimationsModule,
     MatSliderModule,
     MatButtonModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
